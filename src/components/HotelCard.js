669e7fa8843f96830c41e94909bda26a1ef8fc1f
Edward Achike <edwardachike@gmail.com>
2022-12-05 16:55:19 +0100

    fix linter errors

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const HotelCard = () => (
-  <div className="hotel-card">
-    
-  </div>
-)
-  
+  <div className="hotel-card" />
+);
+
+export default HotelCard;
