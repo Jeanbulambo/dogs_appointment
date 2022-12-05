be4ca4fc735585fde6e30f82ae1cf74d7477dd76
Jean Bulambo <jeanbulambo4@gmail.com>
2022-12-05 18:41:58 +0200

    Add Home card

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const HotelCard = () => (
-  <div className="hotel-card" />
-);
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function HotelCard() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Home Hotel</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">My profile</Button>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default HotelCard;
