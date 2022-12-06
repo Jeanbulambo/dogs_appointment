2626beea826ebddfcefa2a15a843b57791b9d1b7
Jean Bulambo <jeanbulambo4@gmail.com>
2022-12-06 19:52:50 +0200

    Add style for mobile version

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -9,20 +9,24 @@ function HotelCard() {
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Home Hotel</Card.Title>
+        <div className="d-flex flex-column justify-content-center">
+          <Card.Title className="text-center display-4 fw-bold">Availlable pets</Card.Title>
+          <p className="text-center h6" style={{ height: '3rem' }}>Please select your choice</p>
+        </div>
         <Card.Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          <img src="cinqueterre.jpg" className="rounded-circle" alt="Cinque Terre" />
         </Card.Text>
-        <ul className="hotel-socials">
-          <li><img src="" alt="twitter" /></li>
-          <li><img src="" alt="facebook" /></li>
-          <li><img src="" alt="google+" /></li>
-          <li><img src="" alt="v" /></li>
-          <li><img src="" alt="p" /></li>
-        </ul>
+        <h3 className="text-center">PET</h3>
         <Button variant="primary">My profile</Button>
+        <p className="text-center h6" style={{ height: '3rem', margin: '1rem' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <ul className="hotel-socials">
+          <li><img src="" alt="twit" /></li>
+          <li><img src="" alt="faceb" /></li>
+          <li><img src="" alt="google+" /></li>
+        </ul>
       </Card.Body>
     </Card>
   );
