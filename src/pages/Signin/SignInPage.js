f53306d24647752cf809d840ccd671cd9d3ec494
Edward Achike <edwardachike@gmail.com>
2022-12-14 11:15:32 +0100

    j

--- src/pages/Signin/SignInPage.js
+++ src/pages/Signin/SignInPage.js
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import SignIn from '../../components/SignIn/SignIn';
 import './signinpage.css';
 
 const SignInPage = () => (
   <div className="signin-container">
-    <div id="bg-image-signin"></div>
+    <div id="bg-image-signin" />
     <SignIn />
   </div>
-  
+
 );
 
 export default SignInPage;
