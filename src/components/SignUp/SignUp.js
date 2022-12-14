888e0caa6ea9666bad4726b47ebbd2cab3f2cbc2
Jean Bulambo <jeanbulambo4@gmail.com>
2022-12-14 15:07:40 +0200

    Styling signup form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -3,6 +3,7 @@ import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { postSignUp } from '../../redux/registration/registration';
+import './signUp.css';
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -27,43 +28,56 @@ const SignUp = () => {
 
   return (
     <div>
-      <div>
-        <h1>Sign Up</h1>
+      <div >
         <form ref={formRef} onSubmit={handleSubmit}>
+         <div class="container">
+           <h1>Register</h1>
+    <p>Please fill in this form to create an account.</p>
+    <label for="name"><b>Name</b></label>
           <input
             placeholder="Name"
             type="text"
             name="name"
+            id="name"
           />
-
+          <label for="phone"><b>Phone</b></label>
           <input
             placeholder="Phone"
             type="text"
             name="phone"
+            id="phone"
           />
 
+         <label for="email"><b>Email</b></label>
           <input
             placeholder="Email"
-            type="email"
+            type="text"
             name="email"
+            id="email"
           />
 
+          <label for="psw"><b>Password</b></label>
           <input
             placeholder="Password"
             type="password"
             name="password"
+            id="psw"
           />
 
+          <label for="psw-repeat"><b>Repeat Password</b></label>
           <input
             placeholder="Password confirmation"
             type="password"
             name="password_confirmation"
+            id="psw-repeat"
           />
+          <hr />
 
-          <input type="submit" value="Sing Up" />
+          <input type="submit" value="Sing Up" class="registerbtn"/>
 
           <div>
             <Link to="/">Sign In</Link>
+          </div>
           </div>
         </form>
       </div>
