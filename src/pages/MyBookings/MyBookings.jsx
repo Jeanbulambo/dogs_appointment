bad1dffdd72bab47c2238dee9f3a9d4825e6b8df
aron-helu <aronmilahela@gmail.com>
2022-12-15 22:27:58 +0300

    Delet commented folder

--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import ListBookings from '../../components/ListBookings/ListBookings';
+import '../../components/ListBookings/ListBookings.css';
 
 const MyBookings = () => (
-  <div>
+  <div className="container">
     <ListBookings />
   </div>
 );
